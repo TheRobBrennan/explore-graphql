5779151e639978c91b7b2dc0baf591ad02092e27
Rob Brennan <rob@therobbrennan.com>
2019-11-05 15:40:56 -0800

    Added optional Apollo Engine support

--- back-end/javascript/apollo-server/example-using-mongodb-and-postgresql/src/index.js
+++ back-end/javascript/apollo-server/example-using-mongodb-and-postgresql/src/index.js
@@ -3,12 +3,18 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 // Apollo server
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
+const apolloEngine = require('./apollo/engine-demo');
+const engine = (process.env.ENGINE_API_KEY) ? {...apolloEngine, apiKey: process.env.ENGINE_API_KEY} : {}
 
 // Start our server
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine,
+});
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
