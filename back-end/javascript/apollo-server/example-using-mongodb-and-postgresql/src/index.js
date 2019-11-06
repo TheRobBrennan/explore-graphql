f9334bd29edebc4ee885fcf171f7f950d2be48a1
Rob Brennan <rob@therobbrennan.com>
2019-11-05 16:05:21 -0800

    Added support for Apollo graph manager

--- back-end/javascript/apollo-server/example-using-mongodb-and-postgresql/src/index.js
+++ back-end/javascript/apollo-server/example-using-mongodb-and-postgresql/src/index.js
@@ -7,6 +7,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 const apolloEngine = require('./apollo/engine-demo');
+// If you are using the Apollo Graph Manager, be sure to define ENGINE_API_KEY in your .env file
 const engine = (process.env.ENGINE_API_KEY) ? {...apolloEngine, apiKey: process.env.ENGINE_API_KEY} : {}
 
 // Start our server
