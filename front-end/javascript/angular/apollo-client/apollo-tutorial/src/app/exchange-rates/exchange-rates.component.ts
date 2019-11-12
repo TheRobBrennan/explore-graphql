0ef186fd9a8e2a25c5bdd63781a8dd7ca85418ca
Rob Brennan <rob@therobbrennan.com>
2019-11-12 13:29:19 -0800

    Ta-da - here is a working GraphQL call in Angular

--- front-end/javascript/angular/apollo-client/apollo-tutorial/src/app/exchange-rates/exchange-rates.component.ts
+++ front-end/javascript/angular/apollo-client/apollo-tutorial/src/app/exchange-rates/exchange-rates.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
+import { shareReplay, map } from 'rxjs/operators';
+import { ExchangeRate, ExchangeRatesQuery } from './exchange-rates';
 
 @Component({
   selector: 'app-exchange-rates',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exchange-rates.component.css']
 })
 export class ExchangeRatesComponent implements OnInit {
+  rates$: Observable<ExchangeRate[]>;
+  loading$: Observable<boolean>;
+  errors$: Observable<any>;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-  }
+    const query = gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `;
 
+    // The watchQuery method returns a QueryRef object which has the valueChanges property that is an Observable
+    const source$ = this.apollo.watchQuery<ExchangeRatesQuery>({query})
+      .valueChanges
+      .pipe(shareReplay(1));
+
+    // Pipe source$ to our Observables
+    this.rates$ = source$.pipe(map(result => result.data && result.data.rates));
+    this.loading$ = source$.pipe(map(result => result.loading));
+    this.errors$ = source$.pipe(map(result => result.errors));
+  }
 }
